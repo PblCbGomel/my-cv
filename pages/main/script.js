b418d093322fae4dc1df28b882e99d3b07bf4ed8
PblCbGomel <yevgeniy.shevtsov.pblcb@mail.ru>
2022-03-30 00:24:59 +0300

    feat: add interactive and anchors

--- pages/main/script.js
+++ pages/main/script.js
@@ -4,3 +4,15 @@ discordContact.addEventListener('click', () => {
     navigator.clipboard.writeText('Envy#8637')
     alert('Discord ID(added to clipboard): Envy#8637')
 })
+
+const avatar = document.querySelector('.avatar')
+
+avatar.addEventListener('mouseenter', () => {
+    avatar.classList.add('avatar-me')
+    avatar.classList.remove('avatar-lynx')
+})
+
+avatar.addEventListener('mouseleave', () => {
+    avatar.classList.add('avatar-lynx')
+    avatar.classList.remove('avatar-me')
+})
